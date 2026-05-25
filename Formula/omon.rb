bf7d77b3c1d877a00294432e4964e18715f37f61
Sebastien Taggart <sebastien.taggart@gmail.com>
2026-05-25 16:38:36 -0400

    omon 0.6.1

--- Formula/omon.rb
+++ Formula/omon.rb
@@ -3,8 +3,8 @@ class Omon < Formula
 
   desc "Local-first monitoring and management tool for Ollama"
   homepage "https://github.com/LightbridgeLab/OllamaMon"
-  url "https://github.com/LightbridgeLab/OllamaMon/archive/refs/tags/v0.5.0.tar.gz"
-  sha256 "ffc521ec1f449e2a4a93b303f7e7519acf8523cdf853d1c31ebd116cfde28df5"
+  url "https://github.com/LightbridgeLab/OllamaMon/archive/refs/tags/v0.6.1.tar.gz"
+  sha256 "6f5be22d40ab6232ba565bb7fba7a15d2f8376a8ecbd2f68d6f9ee69cef1bcc5"
   license "MIT"
 
   depends_on "python@3.13"
